b9e6a1166a18350c6bf06b743032567b06a96281
benwalkerit <benrwalker@icloud.com>
2023-07-16 22:24:00 +0100

    complete.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # portfolio-typing_speed_test
-Typing speed test app
+Typing speed test app. The app will show the user a window, when clicking start, the app will generate some random words and the user must type as fast as they can.
+
+The app will calculate the speed in words per minute and the accuracy of the typing.
+
+
--- main.py
+++ main.py
@@ -1,64 +1,174 @@
-from wonderwords import RandomSentence
-import random
+import pygame
+from pygame.locals import *
+import sys
 import time
-import tkinter
-import ttkthemes
-import ctypes
+from essential_generators import DocumentGenerator
 
-ctypes.windll.shcore.SetProcessDpiAwareness(1)
+class Game:
+    def __init__(self):
+        self.w=750
+        self.h=500
+        self.reset=True
+        self.active = False
+        self.input_text=''
+        self.word = ''
+        self.time_start = 0
+        self.total_time = 0
+        self.accuracy = '0%'
+        self.results = 'Time:0 Accuracy:0 % Wpm:0 '
+        self.wpm = 0
+        self.end = False
+        self.HEAD_C = (153,204,255)
+        self.TEXT_C = (240,240,240)
+        self.RESULT_C = (255,70,70)
 
-window = Tk()
-window.title('Type Speed Test')
 
-window.geometry('700x700')
+        pygame.init()
+        self.open_img = pygame.image.load('image.png')
+        self.open_img = pygame.transform.scale(self.open_img, (self.w,self.h))
 
-window.option_add("*Label.Font", "consolas 30")
-window.option_add("*Button.Font", "consolas 30")
 
-sent_list = []
-sent_paragraph = ""
+        self.bg = pygame.image.load('bg.png')
+        self.bg = pygame.transform.scale(self.bg, (500,750))
 
-for i in range(5):
-    sent = RandomSentence()
-    random_sent = sent.sentence()
-    sent_list.append(random_sent)
-    sent_paragraph += random_sent + " "
+        self.screen = pygame.display.set_mode((self.w,self.h))
+        pygame.display.set_caption('Type Speed test')
+        
+        
+    def draw_text(self, screen, msg, y ,fsize, color):
+        font = pygame.font.Font(None, fsize)
+        text = font.render(msg, 1,color)
+        text_rect = text.get_rect(center=(self.w/2, y))
+        screen.blit(text, text_rect)
+        pygame.display.update()
+    
+    #Generate random sentence.
+    def get_sentence(self):
+        gen = DocumentGenerator()
+        sentence = ''
+        for i in range(1, 12):
+            sentence += gen.word() + ' '
+        print(sentence)
+        return sentence
+    
+    #Show test results, speed, accuracy and words per minute.
+    def show_results(self, screen):
+        if(not self.end):
+            #Calculate time
+            self.total_time = time.time() - self.time_start
 
-def error_rate(sent_paragraph, typed_paragraph):
-    error_count = 0
-    length = len(sent_paragraph)
+            #Calculate accuracy
+            count = 0
+            for i,c in enumerate(self.word):
+                try:
+                    if self.input_text[i] == c:
+                        count += 1
+                except:
+                    pass
+            self.accuracy = count/len(self.word)*100
 
-    for i, char in enumerate(sent_paragraph):
-        try:
-            if char != typed_paragraph[i]:
-                error_count += 1
-        except:
-            error_count += 1
+            #Calculate words per minute
+            self.wpm = len(self.input_text)*60/(5*self.total_time)
+            self.end = True
+            print(self.total_time)
 
-    error_percent = error_count / length * 100
-    return error_percent
+            self.results = 'Time:'+str(round(self.total_time)) +" secs Accuracy:"+ str(round(self.accuracy)) + "%" + ' Wpm: ' + str(round(self.wpm))
 
-def main():
-    print("Type the below paragraph as quickly as possible with as few mistakes to get a high score: \n")
-    print(sent_paragraph)
-    print("\n")
+            # draw icon image
+            self.time_img = pygame.image.load('icon.png')
+            self.time_img = pygame.transform.scale(self.time_img, (150,150))
+            #screen.blit(self.time_img, (80,320))
+            screen.blit(self.time_img, (self.w/2-75,self.h-140))
+            self.draw_text(screen,"Reset", self.h - 70, 26, (100,100,100))
 
-    start_time = time.time()
-    typed_paragraph = input()
-    end_time = time.time()
+            print(self.results)
+            pygame.display.update()
 
-    time_taken = end_time - start_time
+    def run(self):
+        self.reset_game()
 
-    error_percent = error_rate(sent_paragraph, typed_paragraph)
-    print("\n")
 
-    if error_percent >= 50:
-        print(f"Your error rate {error_percent} was quite high and hence your accurate speed could not be computed.")
-    else:
-        speed = len(typed_paragraph) / time_taken
-        print("***** YOUR SCORE REPORT *****")
-        print(f"Your speed is {speed} words/sec")
-        print(f"The error rate is {error_percent}")
+        self.running=True
+        while(self.running):
+            clock = pygame.time.Clock()
+            self.screen.fill((0,0,0), (50,250,650,50))
+            pygame.draw.rect(self.screen,self.HEAD_C, (50,250,650,50), 2)
+            # update the text of user input
+            self.draw_text(self.screen, self.input_text, 274, 26,(250,250,250))
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.running = False
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    x,y = pygame.mouse.get_pos()
+                    # position of input box
+                    if(x>=50 and x<=650 and y>=250 and y<=300):
+                        self.active = True
+                        self.input_text = ''
+                        self.time_start = time.time()
+                    # position of reset box
+                    if(x>=310 and x<=510 and y>=390 and self.end):
+                        self.reset_game()
+                        x,y = pygame.mouse.get_pos()
+                elif event.type == pygame.KEYDOWN:
+                    if self.active and not self.end:
+                        if event.key == pygame.K_RETURN:
+                            print(self.input_text)
+                            self.show_results(self.screen)
+                            print(self.results)
+                            self.draw_text(self.screen, self.results,350, 28, self.RESULT_C)
+                            self.end = True
 
-if __name__ == '__main__':
-    main()
+                        elif event.key == pygame.K_BACKSPACE:
+                            self.input_text = self.input_text[:-1]
+                        else:
+                            try:
+                                self.input_text += event.unicode
+                            except:
+                                pass
+
+            pygame.display.update()
+
+
+        clock.tick(60)
+
+    def reset_game(self):
+        #Blank the screen with black before loading the starting image.
+        self.screen.fill((0,0,0))
+        pygame.display.update()
+        time.sleep(0.5)
+        #Reload app by loading the starting image
+        self.screen.blit(self.open_img, (0,0))
+
+        pygame.display.update()
+        time.sleep(1)
+        #Reset the game variables
+        self.reset=False
+        self.end = False
+
+        self.input_text=''
+        self.word = ''
+        self.time_start = 0
+        self.total_time = 0
+        self.wpm = 0
+
+        # Get random sentence
+        self.word = self.get_sentence()
+        if (not self.word): self.reset_game()
+        #drawing heading
+        self.screen.fill((0,0,0))
+        self.screen.blit(self.bg,(0,0))
+        msg = "Typing Speed Test"
+        self.draw_text(self.screen, msg,80, 80,self.HEAD_C)
+        sub_msg = "Please type the sentence in the box below and press 'ENTER'"
+        self.draw_text(self.screen,sub_msg, 150, 20, self.HEAD_C)
+        # draw the rectangle for input box
+        pygame.draw.rect(self.screen,(255,192,25), (50,250,650,50), 2)
+        # draw the sentence string
+        self.draw_text(self.screen, self.word,200, 28,self.TEXT_C)
+
+        pygame.display.update()
+
+
+Game().run()
